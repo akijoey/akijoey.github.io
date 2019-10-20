aace5dcb3d0ff428b07a8325b8159dfdebda92ee
AkiJoey <akijoey1010635951@gmail.com>
2019-10-21 00:11:36 +0800

    Site updated: 2019-10-21 00:11:16

--- js/main.js
+++ js/main.js
@@ -584,3 +584,61 @@ $(function () {
     $('body').css('font-size', font_size_record - 1)
   });
 });
+
+//首页下雨特效
+$(document).ready(function()
+{
+	var canvas = $('#rain')[0];
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	if (canvas.getContext)
+	{
+		var ctx = canvas.getContext('2d');
+		var w = canvas.width;
+		var h = canvas.height;
+		ctx.strokeStyle = 'rgba(255, 255, 255, 0.2)';
+		ctx.lineWidth = 1;
+		ctx.lineCap = 'round';
+		var init = [];
+		var maxParts = 80;
+		for (var a = 0;a < maxParts;a++)
+			init.push({
+				x: Math.random() * w,
+				y: Math.random() * h,
+				l: Math.random() * 8,
+				xs: Math.random() - 0.3,
+				ys: Math.random() * 10 + 10
+			});
+		var particles = [];
+		for (var b = 0;b < maxParts;b++)
+			particles[b] = init[b];
+		function draw()
+		{
+			ctx.clearRect(0, 0, w, h);
+			for (var c = 0;c < particles.length;c++)
+			{
+				var p = particles[c];
+				ctx.beginPath();
+				ctx.moveTo(p.x, p.y);
+				ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+				ctx.stroke();
+			}
+			move();
+		}
+		function move()
+		{
+			for (var b = 0;b < particles.length;b++)
+			{
+				var p = particles[b];
+				p.x += p.xs;
+				p.y += p.ys;
+				if (p.x > w || p.y > h)
+				{
+					p.x = Math.random() * w;
+					p.y = -20;
+				}
+			}
+		}
+		setInterval(draw, 10);
+	}
+});
